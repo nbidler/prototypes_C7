1292ba64417b9320f5a4bf208f0abfed4a06a742
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-01-26 14:51:33 -0800

    js event delegation - Eric Johnson FS2

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,10 +1,17 @@
 // Answer the questions here:
-
+/*
 // - What do you think is going to happen?
-
+I think it will output "Delegated Button#5 Handler" to the console
 // - What happened?
 
 // - Why?
-
+*/
 //========== Write your code below ===========//
 
+$(document).ready(function() {
+    $('#list').on('click', 'button', function () {
+        console.log($(this).text());
+    });
+
+    $('#list').append("<li><button style='margin-top: 10px'>Delegated Button#5 Handler</button></li>");
+});
