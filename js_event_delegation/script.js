a316e30a57f9eda1d8744cce371b045ddc573bd2
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-01-26 14:53:33 -0800

    js event delegation - Eric Johnson FS2

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -3,8 +3,10 @@
 // - What do you think is going to happen?
 I think it will output "Delegated Button#5 Handler" to the console
 // - What happened?
-
+Exactly what I thought it would.
 // - Why?
+The parent handles all clicks on its child the same way, ergo, even if a new child
+is put in at 'compile time,' it s still handled the same.
 */
 //========== Write your code below ===========//
 
