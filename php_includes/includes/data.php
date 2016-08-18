fef49711e3355539e17d8bc3410d55052882f142
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-18 16:25:17 -0700

    PHP Includes - Nick Bidler

---
+++ php_includes/includes/data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 7/29/2016
+ * Time: 4:51 PM
+ */
+$a = 3;
+$b = 2;
+?>
---
+++ php_includes/includes/data2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 7/29/2016
+ * Time: 4:53 PM
+ */
+$c = $c * 2;
+?>
