a402c99204ea1bda96db4d73d6027052353200da
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-19 16:22:50 -0700

    feature set 1

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -1,1 +1,14 @@
+<?php
+session_start();
+?>
+
 <!-- Session Reader -->
+
+<?php
+    print('name: ');print($_GET['name'].'<br>');
+    print('age: ');print($_GET['age'].'<br>');
+    print('occupation: ');print($_GET['occupation']);
+    $_SESSION['name'] = $_GET['name'];
+    $_SESSION['age'] = $_GET['age'];
+    $_SESSION['occupation'] = $_GET['occupation'];
+?>
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -1,1 +1,34 @@
+<?php
+session_start();
+?>
 <!-- Session Setter -->
+<?php
+
+$prev_name = '';
+if (!empty($_SESSION['name']))
+{
+    $prev_name = $_SESSION['name'];
+}
+
+$prev_age = '';
+if (!empty($_SESSION['age']))
+{
+    $prev_age = $_SESSION['age'];
+}
+
+$prev_occupation = '';
+if (!empty($_SESSION['occupation']))
+{
+    $prev_occupation = $_SESSION['occupation'];
+}
+
+?>
+
+
+
+<form action="session_reader.php">
+    <input type="text" name="name" value=<?=$prev_name?>>
+    <input type="number" name="age" value=<?=$prev_age?>>
+    <input type="text" name="occupation" value=<?=$prev_occupation?>>
+    <input type="submit">
+</form>
