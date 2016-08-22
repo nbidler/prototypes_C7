41a0663e72c75b6fc0556cc7ad50d2600b283bee
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-22 15:37:17 -0700

    FS2 Nick Bidler

--- php_mysql/index_select.php
+++ php_mysql/index_select.php
@@ -1,1 +1,13 @@
 <!-- index_select.php -->
+<?php
+require('mysql_connect.php');
+
+$query = "SELECT * FROM `todo_items` WHERE `user_id` = 333";
+
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result)){
+    print_r(mysqli_fetch_assoc($result));
+}
+
+?>
--- php_mysql/mysql_connect.php
+++ php_mysql/mysql_connect.php
@@ -1,1 +1,3 @@
-$conn =mysqli_connect("YOUR SERVER ADDRESS", "YOUR USERNAME", "YOUR PASSWORD", "YOUR DATABASE");
+<?php
+$conn =mysqli_connect("localhost", "root", "root", "testdb");
+?>
