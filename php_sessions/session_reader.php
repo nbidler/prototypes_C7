b9292ddca4834d000a87c325576bc459175f554e
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-21 18:34:42 -0700

    i am missing something

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -4,11 +4,64 @@ session_start();
 
 <!-- Session Reader -->
 
+<script>
+
+    var inName = '<?php print($_GET['name']); ?>';
+
+    if (1+ inName.search(/^[A-Za-z]{1,} *[A-Za-z]*$/)){
+        console.log('name ' + '<?php print($_GET['name']); ?>');
+        <?php
+            //print('name: ');print('$_GET["name"]');
+        ?>
+        <?php
+            $_SESSION['name'] = $_GET['name'];
+        ?>
+    }
+    else {
+        console.log('name err ' + '<?php print($_GET['name']); ?>');
+        <?php
+        $_SESSION['errors']['name'] = $_GET['name'];
+        ?>
+    }
+
+    var inAge = '<?php print($_GET['age']); ?>';
+    if (1+ inAge.search(/^[0-9]{1,}$/)){
+        console.log('age ' + '<?php print($_GET['age']); ?>');
+        <?php
+        //print('age: ');print('$_GET["age"]');
+        $_SESSION['age'] = $_GET['age'];
+        ?>
+    }
+    else {
+        console.log('name err ' + '<?php print($_GET['name']); ?>');
+        <?php
+        $_SESSION['errors']['age'] = $_GET['age'];
+        ?>
+    }
+
+    var inJob = '<?php print($_GET['occupation']); ?>';
+    if (1+ inJob.search(/^[A-Za-z]{1,} *[A-Za-z]*$/)){
+        console.log('occupation ' + '<?php print($_GET['occupation']); ?>');
+        <?php
+        //print('name: ');print('$_GET["name"]');
+        ?>
+        <?php
+        $_SESSION['occupation'] = $_GET['occupation'];
+        ?>
+    }
+    else {
+        console.log('name err ' + '<?php print($_GET['name']); ?>');
+        <?php
+        $_SESSION['errors']['occupation'] = $_GET["occupation"];
+        ?>
+    }
+
+</script>
+
 <?php
-    print('name: ');print($_GET['name'].'<br>');
-    print('age: ');print($_GET['age'].'<br>');
-    print('occupation: ');print($_GET['occupation']);
-    $_SESSION['name'] = $_GET['name'];
-    $_SESSION['age'] = $_GET['age'];
-    $_SESSION['occupation'] = $_GET['occupation'];
-?>
+header('location: session_setter.php');
+
+//    print('name: ');print($_GET['name'].'<br>');
+//    print('age: ');print($_GET['age'].'<br>');
+//    print('occupation: ');print($_GET['occupation']);
+//?>
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -4,23 +4,43 @@ session_start();
 <!-- Session Setter -->
 <?php
 
-$prev_name = '';
+echo $_SESSION['errors']['name'];
+echo $_SESSION['errors']['age'];
+echo $_SESSION['errors']['occupation'];
+
+$prev_name = ''; $err_name = '';
+
 if (!empty($_SESSION['name']))
 {
     $prev_name = $_SESSION['name'];
+    if (!empty($_SESSION['name'])){
+        $err_name = $_SESSION['errors']['name'];
+    }
 }
 
-$prev_age = '';
+$prev_age = ''; $err_age = '';
+
 if (!empty($_SESSION['age']))
 {
     $prev_age = $_SESSION['age'];
+    if (!empty($_SESSION['age'])){
+        $err_age = $_SESSION['errors']['age'];
+    }
 }
 
-$prev_occupation = '';
+$prev_occupation = ''; $err_occupation = '';
+
 if (!empty($_SESSION['occupation']))
 {
     $prev_occupation = $_SESSION['occupation'];
+    if (!empty($_SESSION['occupation'])){
+        $err_occupation = $_SESSION['errors']['occupation'];
+    }
 }
+
+$_SESSION['errors']['name'] = '';
+$_SESSION['errors']['age'] = '';
+$_SESSION['errors']['occupation'] = '';
 
 ?>
 
@@ -28,7 +48,10 @@ if (!empty($_SESSION['occupation']))
 
 <form action="session_reader.php">
     <input type="text" name="name" value=<?=$prev_name?>>
+    <p><?= $err_name ?></p>
     <input type="number" name="age" value=<?=$prev_age?>>
+    <p><?= $err_age ?></p>
     <input type="text" name="occupation" value=<?=$prev_occupation?>>
+    <p><?= $err_occupation ?></p>
     <input type="submit">
 </form>
