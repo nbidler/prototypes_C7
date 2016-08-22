33035835ed5a8592fd3f5e2add0806be08d0b835
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-22 13:12:41 -0700

    FS1 Nick Bidler

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,11 @@
-Place your queries below
+-- Place your queries below
+
+-- FS1
+
+SELECT * FROM `students` WHERE `username`='dd'
+
+UPDATE `students` SET `email`='4d4d4d' WHERE `Name`='dd'
+
+INSERT INTO `students` SET `Name`='Mandy', `email`='greatemail@email.com', `password`=sha1('thisisagreatpassword'), `age`=12, `ID`='Mandy12'
+
+DELETE FROM `students` WHERE `Name`='Mandy'
