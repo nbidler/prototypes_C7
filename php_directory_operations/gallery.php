c1889b0c77ee912b83e47f4d5244af72edcc2703
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-22 09:47:35 -0700

    feature set 1 + some 2

---
+++ php_directory_operations/gallery.php
@@ -0,0 +1,27 @@
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <script>
+        function load_files() {
+            $.getJSON('get_images.php',,
+                function(data,status0){
+                    for (var i = 0; i < )
+                });
+        }
+    </script>
+
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: Nick
+     * Date: 8/21/2016
+     * Time: 9:04 PM
+     */
+    ?>
+
+</head>
+<body>
+
+
+</body>
--- php_directory_operations/get_images.php
+++ php_directory_operations/get_images.php
@@ -6,14 +6,33 @@
  * Time: 2:02 PM
  */
 
-$picute = glob('images/*');
+$picute = glob('images\*');
 
-foreach ($picute as $item){
-    //print("<img src='$item'><br>");
+$output = [
+    'success' => array(),
+    'error' => array(),
+    'files' => array()
+];
 
-    $output = [
-        $success => '',
-        $error => '',
-        $files => ''
-    ];
+foreach ($picute as $item) {
+    //print("<img src='$item'><br>"
+    if (is_file($item)) {
+        if (is_readable($item)) {
+            $output['success'][] = $item;
+        } else {
+            $output['error'][] = $item;
+        }
+        $output['files'][] = $item;
+    }
 }
+
+print_r($output['success']);
+print("<br>");
+print_r($output['error']);
+print("<br>");
+print_r($output['files']);
+print("<br>");
+
+$enc_output = json_encode($output);
+
+print_r($enc_output);
