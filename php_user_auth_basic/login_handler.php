12b70532ddd71919d443818e1fc2ec9270091351
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-24 15:55:23 -0700

    PHP User Auth - Nick Bidler

--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -3,15 +3,15 @@
 session_start();
 
 $user_info = [
-    ['id'=> 1, 'username'=>'alpha', 'password' => 'alph'],
-    ['id'=> 2, 'username'=>'beta', 'password' => 'bet'],
-    ['id'=> 3, 'username'=>'charlie', 'password' => 'charli']
+    ['id'=> 1, 'username'=>'alpha', 'password' => '52e51c43e283548b762e3c9dd9a4600768db9fb4'],
+    ['id'=> 2, 'username'=>'beta', 'password' => 'c033816b54c9eccffff834e492c66a262938ee18'],
+    ['id'=> 3, 'username'=>'charlie', 'password' => '4ff053e82dc70e9c6a22544252ba01befdc340c0']
 ];
 
 $output = [];
 
 $input_username = $_POST['username'];
-$input_password = $_POST['password'];
+$input_password = sha1($_POST['password']);
 $login_failed = true;
 
 foreach ($user_info as $entry)
