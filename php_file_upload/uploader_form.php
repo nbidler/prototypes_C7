5f43da1c5d536b1f420d22542c20509290b0e5c1
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-08-24 16:56:06 -0700

    FS1 Nick Bidler

--- php_file_upload/uploader_form.php
+++ php_file_upload/uploader_form.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html>
 <head>
+
 </head>
 <body>
-	<form id="file_upload">
-		
+	<form id="file_upload" enctype="multipart/form-data" action="file_handler.php" method="post">
+		<input type="file" name="upload_file">
+		<input type="text" name="description">
+		<button type="submit">Send</button>
 	</form>
 </body>
 </html>
